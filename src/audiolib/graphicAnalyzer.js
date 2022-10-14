2320ddb6b1acb69e6072b2b9702e24265ffcb367
mindcitycode <mindcity@proton.me>
2022-10-14 14:36:21 +0200

    audiolib

---
+++ src/audiolib/graphicAnalyzer.js
@@ -0,0 +1,89 @@
+import { rafLoop } from "./lib/loop"
+
+export const updateView = (ctx, analyser, dataArray) => {
+
+    analyser.getByteTimeDomainData(dataArray)
+    ctx.fillStyle = 'rgba(128,128,128,0.5)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.lineWidth = 4
+    ctx.strokeStyle = 'black'
+    ctx.beginPath()
+
+    const sliceWidth = canvas.width / bufferLength
+
+    let x = 0
+    for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = v * (canvas.height / 2);
+
+        if (i === 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+    ctx.stroke()
+
+}
+
+export const Oscilloscope = ac => {
+
+    const analyser = ac.createAnalyser();
+    analyser.fftSize = 256 * 8;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const canvas = document.createElement('canvas')
+    canvas.width = 400
+    canvas.height = 200
+    document.body.append(canvas)
+    const ctx = canvas.getContext('2d')
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    const loopState = rafLoop((dt, t) => {
+        updateView(ctx, analyser, dataArray)
+    })
+    /*
+      analyser.getByteTimeDomainData(dataArray)
+      ctx.fillStyle = 'rgba(128,128,128,0.5)'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+      ctx.lineWidth = 4
+      ctx.strokeStyle = 'black'
+      ctx.beginPath()
+
+      const sliceWidth = canvas.width / bufferLength
+
+      let x = 0
+      for (let i = 0; i < bufferLength; i++) {
+          const v = dataArray[i] / 128.0;
+          const y = v * (canvas.height / 2);
+
+          if (i === 0) {
+              ctx.moveTo(x, y);
+          } else {
+              ctx.lineTo(x, y);
+          }
+
+          x += sliceWidth;
+      }
+      ctx.stroke()
+
+  })
+
+*/
+
+    /*   const osc = ac.createOscillator()
+       osc.frequency.value = 256;
+       osc.start()
+       osc.connect(analyser)
+   */
+    //    analyser.connect(ac.destination)
+
+    return {
+        nodes: { analyser },
+        input: analyser
+    }
+}
--- src/lib/loop.js
+++ src/lib/loop.js
@@ -1,11 +1,13 @@
 export const rafLoop = f => {
+    let state = { halt: true }
     const frame = () => {
         const time = performance.now()
         const dt = time - lastTime
-        f(dt/1000,time/1000)
+        f(dt / 1000, time / 1000)
         lastTime = time
-        requestAnimationFrame(frame)
+        if (!state.halt) requestAnimationFrame(frame)
     }
     let lastTime = performance.now()
     requestAnimationFrame(frame)
+    return state
 }
